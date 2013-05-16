0d49435806937fc1cf98e191d0e6ad894dd07996
Michael Chang <thenewme91@gmail.com>
2013-05-16 17:04:26 -0400

    use file logging instead

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import multiprocessing
 import os
 import select
@@ -13,15 +14,11 @@ log = []
 
 @app.route("/")
 def hello():
-    while True:
-        line = queue.get(False)
-        if line is None or line == "":
-          break;
-        log.append(line)
-    log = log[-100:]
+    f = open("build.log", "r")
+    log = f.readlines()
     return json.dumps(log)
 
-def build(queue):
+def build():
     os.chdir("/home/cryptkeeper/src")
     popen = subprocess.Popen("git clone git://github.com/zmanji/ecryptfs.git",
                              shell=True, bufsize=4096, stdin=None,
@@ -34,11 +31,12 @@ def build(queue):
         line = result[0][0].readline()
         if not line:
             break;
-        queue.put(line)
+        logging.info(line)
 
 if __name__ == "__main__":
-    queue = multiprocessing.Queue()
-    process = multiprocessing.Process(target=build, args=(queue,))
+    logging.basicConfig(filename='build.log',level=logging.DEBUG)
+
+    process = multiprocessing.Process(target=build, args=())
 
     process.start()
 

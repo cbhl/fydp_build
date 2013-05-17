3a604add7676bc463f6e39c51708543013e5c833
Michael Chang <thenewme91@gmail.com>
2013-05-17 16:07:52 -0400

    Update log periodically.

--- static/build.js
+++ static/build.js
@@ -2,17 +2,27 @@ $(function() {
     var $logDiv = $('<div class="log">');
     $("body").append($logDiv);
 
-    $.ajax({
-        dataType: "json",
-        url: "/log.json"
-    }).then(function(data) {
-        console.log(data);
+    var displayLog = function(data) {
+        $logDiv.empty();
         _.each(data, function(line) {
             $lineDiv = $('<div class="log-line">');
             $lineDiv.text(line);
             $logDiv.append($lineDiv);
         });
-    }, function(a,b) {
-        console.log(b);
-    });
+    };
+
+    var logAjaxError = function(jqXHR, textStatus) {
+        console.log("ERROR: " + textStatus);
+    };
+
+    var updateLog = function() {
+        $.ajax({
+            dataType: "json",
+            url: "/log.json"
+        }).then(displayLog, logAjaxError).then(function() {
+            window.setTimeout(updateLog, 1000);
+        });
+    };
+
+    updateLog();
 });

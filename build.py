5969583b7c1612a0104becb440a2bba2d4b21493
Michael Chang <thenewme91@gmail.com>
2013-05-19 16:29:23 -0400

    Two newlines at the end of each event.

--- build.py
+++ build.py
@@ -28,8 +28,7 @@ def build_log_stream():
          resp_str = "\n"
     else:
         for n in range(last_line+1,log_len):
-            resp_str += "event: buildlog\nid: %d\ndata: %s\n" % (n, log[n].rstrip())
-        resp_str += "\n"
+            resp_str += "event: buildlog\nid: %d\ndata: %s\n\n" % (n, log[n].rstrip())
     response = make_response(resp_str)
     response.headers['Content-Type'] = 'text/event-stream'
     return response

83c5fe73fe361b8ae6ce4a811acd3c4d3529e25e
Michael Chang <thenewme91@gmail.com>
2013-06-06 17:01:50 -0400

    better defaults

--- build.py
+++ build.py
@@ -28,8 +28,8 @@ def status():
         db=0
         )
 
-    result['last-build'] = r.get('last-build') or False
-    result['last-build-duration'] = r.get('last-build-duration') or False
+    result['last-build'] = r.get('last-build')
+    result['last-build-duration'] = r.get('last-build-duration')
     test_results_json = r.get('test-results')
     result['test-results'] = json.loads(test_results_json) if test_results_json else {}
     result['tests_pass'] = r.get('tests-pass') or False
